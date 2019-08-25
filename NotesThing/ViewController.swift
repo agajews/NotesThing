e4501f10a464be4e3accd563b7bc720920d14d18
agajews <agajews@gmail.com>
2019-08-25 17:50:55 -0400

    Initial Commit

---
+++ NotesThing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesThing
+//
+//  Created by Alex Gajewski on 8/25/19.
+//  Copyright © 2019 Alex Gajewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
